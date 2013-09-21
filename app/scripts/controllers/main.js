0c084aa657ffba1793d2f5c5ac8aa611762e4ead
Jason Dodds <jason.dodds@gmail.com>
2013-09-21 12:28:31 -0400

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('sftkAngularApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
