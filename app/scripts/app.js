1e2df6bc7db3a7ff8c6b0e964bc1ac6240090ffb
Jason Dodds <jason.dodds@gmail.com>
2013-09-22 12:59:46 -0400

    incorportated calls to sfdc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,289 @@
 'use strict';
 
-angular.module('sftkAngularApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+var app = angular.module('isamAngularApp', ['AngularForce', 'AngularForceObjectFactory', 'Contact']).
+  config(function ($routeProvider) {
+    $routeProvider.
+      when('/', {controller: 'HomeCtrl', templateUrl: '../views/home.html'}).
+      when('/login', {controller: 'LoginCtrl', templateUrl: '../views/login.html'}).
+      when('/logout', {controller: 'LoginCtrl', templateUrl: '../views/logout.html'}).
+      when('/callback', {controller: 'CallbackCtrl', templateUrl: '../views/callback.html'}).
+      when('/contacts', {controller: 'ContactListCtrl', templateUrl: '../views/contact/list.html'}).
+      when('/view/:contactId', {controller: 'ContactViewCtrl', templateUrl: '../views/contact/view.html'}).
+      when('/edit/:contactId', {controller: 'ContactDetailCtrl', templateUrl: '../views/contact/edit.html'}).
+      when('/new', {controller: 'ContactDetailCtrl', templateUrl: '../views/contact/edit.html'}).
+      when('/accounts', {controller: 'AccountListCtrl', templateUrl: '../views/account/list.html'}).
+      otherwise({redirectTo: '/'});
+
+  }).
+  controller('HomeCtrl', function ($scope, AngularForce, $location) {
+    //var isOnline =  AngularForce.isOnline();
+    var isOnline =  true;
+    var isAuthenticated = AngularForce.authenticated();
+
+    //Offline support (only for Cordova)
+    //First check if we are online, then check if we are already authenticated (usually happens in Cordova),
+    //If Both online and authenticated(Cordova), go directly to /contacts view. Else show login page.
+    if(!isOnline) {
+        if(!isAuthenticated) {//MobileWeb
+            return $location.path('/login');
+        } else {//Cordova
+            return $location.path('/contacts/');
+        }
+    }
+
+    //If in visualforce, directly login
+    if (AngularForce.inVisualforce) {
+        $location.path('/login');
+    } else if (AngularForce.refreshToken) { //If web, try to relogin using refresh-token
+        AngularForce.login(function () {
+            $location.path('/contacts/');
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        });
+    } else {
+        $location.path('/login');
+    }
+}).
+controller('LoginCtrl', function ($scope, AngularForce, $location) {
+    //Usually happens in Cordova
+    if (AngularForce.authenticated()) {
+        return $location.path('/contacts/');
+    }
+
+    $scope.login = function () {
+        //If in visualforce, 'login' = initialize entity framework
+        if (AngularForce.inVisualforce) {
+           AngularForce.login(function() {
+            $location.path('/contacts/');
+           });
+        } else {
+            AngularForce.login();
+        }
+    };
+
+
+
+    $scope.isLoggedIn = function () {
+        return AngularForce.authenticated();
+    };
+
+    $scope.logout = function () {
+        AngularForce.logout(function () {
+            //Now go to logout page
+            $location.path('/logout');
+            $scope.$apply();
+        });
+    };
+}).
+controller('CallbackCtrl', function ($scope, AngularForce, $location) {
+    AngularForce.oauthCallback(document.location.href);
+
+    //Note: Set hash to empty before setting path to /contacts to keep the url clean w/o oauth info.
+    //..coz oauth CB returns access_token in its own hash making it two hashes (1 from angular,
+    // and another from oauth)
+    $location.hash('');
+    $location.path('/contacts');
+}).
+controller('ContactListCtrl', function ($scope, AngularForce, $location, Contact) {
+    if (!AngularForce.authenticated()) {
+        return $location.path('/home');
+    }
+
+    $scope.searchTerm = '';
+    $scope.working = false;
+
+    Contact.query(function (data) {
+        $scope.contacts = data.records;
+        $scope.$apply();//Required coz sfdc uses jquery.ajax
+    }, function (data) {
+        alert('Query Error');
+    });
+
+    $scope.isWorking = function () {
+        return $scope.working;
+    };
+
+    $scope.doSearch = function () {
+        Contact.search($scope.searchTerm, function (data) {
+            $scope.contacts = data;
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        }, function (data) {
+
+        });
+    };
+
+    $scope.doView = function (contactId) {
+        console.log('doView');
+        $location.path('/view/' + contactId);
+    };
+
+    $scope.doCreate = function () {
+        $location.path('/new');
+    };
+}).
+controller('ContactCreateCtrl', function ($scope, $location, Contact) {
+    $scope.save = function () {
+        Contact.save($scope.contact, function (contact) {
+            var c = contact;
+            $scope.$apply(function () {
+                $location.path('/view/' + c.Id);
+            });
+        });
+    };
+}).
+controller('ContactViewCtrl', function ($scope, AngularForce, $location, $routeParams, Contact) {
+
+    AngularForce.login(function () {
+        Contact.get({id: $routeParams.contactId}, function (contact) {
+            self.original = contact;
+            $scope.contact = new Contact(self.original);
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        });
+    });
+
+}).
+controller('ContactDetailCtrl', function ($scope, AngularForce, $location, $routeParams, Contact) {
+    var self = this;
+
+    if ($routeParams.contactId) {
+        AngularForce.login(function () {
+            Contact.get({id: $routeParams.contactId},
+                function (contact) {
+                    self.original = contact;
+                    $scope.contact = new Contact(self.original);
+                    $scope.$apply();//Required coz sfdc uses jquery.ajax
+                });
+        });
+    } else {
+        $scope.contact = new Contact();
+        //$scope.$apply();
+    }
+
+    $scope.isClean = function () {
+        return angular.equals(self.original, $scope.contact);
+    };
+
+    $scope.destroy = function () {
+        self.original.destroy(
+            function () {
+                $scope.$apply(function () {
+                    $location.path('/contacts');
+                });
+            },
+            function (errors) {
+                alert('Could not delete contact!\n' + JSON.parse(errors.responseText)[0].message);
+            }
+        );
+    };
+
+    $scope.save = function () {
+        if ($scope.contact.Id) {
+            $scope.contact.update(function () {
+                $scope.$apply(function () {
+                    $location.path('/view/' + $scope.contact.Id);
+                });
+
+            });
+        } else {
+            Contact.save($scope.contact, function (contact) {
+                var c = contact;
+                $scope.$apply(function () {
+                    $location.path('/view/' + c.Id || c.id);
+                });
+            });
+        }
+    };
+
+    $scope.doCancel = function () {
+        if ($scope.contact.Id) {
+            $location.path('/view/' + $scope.contact.Id);
+        } else {
+            $location.path('/contacts');
+        }
+    };
+});
+
+/**
+ * Describe Salesforce object to be used in the app. For example: Below AngularJS factory shows how to describe and
+ * create an 'Contact' object. And then set its type, fields, where-clause etc.
+ *
+ *  PS: This module is injected into ListCtrl, EditCtrl etc. controllers to further consume the object.
+ */
+angular.module('Contact', []).factory('Contact', function (AngularForceObjectFactory) {
+    var Contact = new AngularForceObjectFactory({type: 'Contact', fields: ['FirstName', 'LastName', 'Title', 'Phone', 'Email', 'Id'], where: '', limit: 20});
+    return Contact;
+});
+
+angular.module('Account', []).factory('Account', function (AngularForceObjectFactory) {
+    var Account = new AngularForceObjectFactory({type: 'Account', fields: ['Id', 'Name', 'BillingCity'], where: '', limit: 50});
+    return Account;
+});
+
+function initApp(options, forcetkClient) {
+    options = options || {};
+    options.loginUrl = SFConfig.sfLoginURL;
+    options.clientId = SFConfig.consumerKey;
+    options.apiVersion = 'v27.0';
+    options.userAgent = 'SalesforceMobileUI/alpha';
+    options.proxyUrl = options.proxyUrl || SFConfig.proxyUrl;
+
+    //In VF, you should get sessionId and use that as accessToken while initializing forcetk(Force.init)
+    if (SFConfig.sessionId) {
+        options.accessToken = SFConfig.sessionId;
+    }
+
+    //Init force
+    Force.init(options, options.apiVersion, forcetkClient);
+}
+
+/**
+ * Please configure Salesforce consumerkey, proxyUrl etc in getSFConfig().
+ *
+ * SFConfig is a central configuration JS Object. It is used by angular-force.js and also your app to set and retrieve
+ * various configuration or authentication related information.
+ *
+ * Note: Please configure SFConfig Salesforce consumerkey, proxyUrl etc in getSFConfig() below.
+ *
+ * @property SFConfig Salesforce Config object with the following properties.
+ * @attribute {String} sfLoginURL       Salesforce login url
+ * @attribute {String} consumerKey      Salesforce app's consumer key
+ * @attribute {String} oAuthCallbackURL OAuth Callback URL. Note: If you are running on Heroku or elsewhere you need to set this.
+ * @attribute {String} proxyUrl         URL to proxy cross-domain calls. Note: This nodejs app acts as a proxy server as well at <location>/proxy/
+ * @attribute {String} client           ForcetkClient. Set by forcetk lib
+ * @attribute {String} sessionId        Session Id. Set by forcetk lib
+ * @attribute {String} apiVersion       REST Api version. Set by forcetk (Set this manually for visualforce)
+ * @attribute {String} instanceUrl      Your Org. specific url. Set by forcetk.
+ *
+ * @returns SFConfig object depending on where (localhost v/s heroku v/s visualforce) the app is running.
+ */
+function getSFConfig() {
+    var location = document.location;
+    var href = location.href;
+    if (href.indexOf('file:') >= 0) { //Phonegap
+        return {};
+    } else if (configFromEnv && configFromEnv.sessionId) { //VisualForce just sets sessionId (as that's all what is required)
+        return {
+            sessionId: configFromEnv.sessionId
+        };
+    } else {
+        if (!configFromEnv || configFromEnv.client_id === '' || configFromEnv.client_id === 'undefined' || configFromEnv.app_url === '' || configFromEnv.app_url === 'undefined') {
+            throw 'Environment variable client_id and/or app_url is missing. Please set them before you start the app';
+        }
+        return {
+            sfLoginURL: 'https://login.salesforce.com/',
+            consumerKey: configFromEnv.client_id,
+            oAuthCallbackURL: removeTrailingSlash(configFromEnv.app_url) + '/#/callback',
+            proxyUrl: removeTrailingSlash(configFromEnv.app_url) + '/proxy/'
+        };
+    }
+}
+
+
+//Helper
+function removeTrailingSlash(url) {
+    return url.replace(/\/$/, '');
+}
+
+var SFConfig = getSFConfig();
+
+SFConfig.maxListSize = 25;
+app.constant('SFConfig', SFConfig);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,190 @@
 'use strict';
 
-angular.module('sftkAngularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('sftkAngularApp', [])
+.controller('HomeCtrl', function ($scope, AngularForce, $location, $route) {
+    var isOnline =  AngularForce.isOnline();
+    var isAuthenticated = AngularForce.authenticated();
+
+    //Offline support (only for Cordova)
+    //First check if we are online, then check if we are already authenticated (usually happens in Cordova),
+    //If Both online and authenticated(Cordova), go directly to /contacts view. Else show login page.
+    if(!isOnline) {
+        if(!isAuthenticated) {//MobileWeb
+            return $location.path('/login');
+        } else {//Cordova
+            return $location.path('/contacts/');
+        }
+    }
+
+    //If in visualforce, directly login
+    if (AngularForce.inVisualforce) {
+        $location.path('/login');
+    } else if (AngularForce.refreshToken) { //If web, try to relogin using refresh-token
+        AngularForce.login(function () {
+            $location.path('/contacts/');
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        });
+    } else {
+        $location.path('/login');
+    }
+})
+
+.controller('LoginCtrl', function ($scope, AngularForce, $location) {
+    //Usually happens in Cordova
+    if (AngularForce.authenticated()) {
+        return $location.path('/contacts/');
+    }
+
+    $scope.login = function () {
+        //If in visualforce, 'login' = initialize entity framework
+        if (AngularForce.inVisualforce) {
+           AngularForce.login(function() {
+            $location.path('/contacts/');
+           });
+        } else {
+            AngularForce.login();
+        }
+    };
+
+
+
+    $scope.isLoggedIn = function () {
+        return AngularForce.authenticated();
+    };
+
+    $scope.logout = function () {
+        AngularForce.logout(function () {
+            //Now go to logout page
+            $location.path('/logout');
+            $scope.$apply();
+        });
+    };
+})
+
+.controller('CallbackCtrl', function ($scope, AngularForce, $location) {
+    AngularForce.oauthCallback(document.location.href);
+
+    //Note: Set hash to empty before setting path to /contacts to keep the url clean w/o oauth info.
+    //..coz oauth CB returns access_token in its own hash making it two hashes (1 from angular,
+    // and another from oauth)
+    $location.hash('');
+    $location.path('/contacts');
+})
+
+.controller('ContactListCtrl', function ($scope, AngularForce, $location, Contact) {
+    if (!AngularForce.authenticated()) {
+        return $location.path('/home');
+    }
+
+    $scope.searchTerm = '';
+    $scope.working = false;
+
+    Contact.query(function (data) {
+        $scope.contacts = data.records;
+        $scope.$apply();//Required coz sfdc uses jquery.ajax
+    }, function (data) {
+        alert('Query Error');
+    });
+
+    $scope.isWorking = function () {
+        return $scope.working;
+    };
+
+    $scope.doSearch = function () {
+        Contact.search($scope.searchTerm, function (data) {
+            $scope.contacts = data;
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        }, function (data) {
+        });
+    };
+
+    $scope.doView = function (contactId) {
+        console.log('doView');
+        $location.path('/view/' + contactId);
+    };
+
+    $scope.doCreate = function () {
+        $location.path('/new');
+    };
+}).
+controller('ContactCreateCtrl', function ($scope, $location, Contact) {
+    $scope.save = function () {
+        Contact.save($scope.contact, function (contact) {
+            var c = contact;
+            $scope.$apply(function () {
+                $location.path('/view/' + c.Id);
+            });
+        });
+    };
+}).
+controller('ContactViewCtrl', function ($scope, AngularForce, $location, $routeParams, Contact) {
+
+    AngularForce.login(function () {
+        Contact.get({id: $routeParams.contactId}, function (contact) {
+            self.original = contact;
+            $scope.contact = new Contact(self.original);
+            $scope.$apply();//Required coz sfdc uses jquery.ajax
+        });
+    });
+
+}).
+controller('ContactDetailCtrl', function ($scope, AngularForce, $location, $routeParams, Contact) {
+    var self = this;
+
+    if ($routeParams.contactId) {
+        AngularForce.login(function () {
+            Contact.get({id: $routeParams.contactId},
+                function (contact) {
+                    self.original = contact;
+                    $scope.contact = new Contact(self.original);
+                    $scope.$apply();//Required coz sfdc uses jquery.ajax
+                });
+        });
+    } else {
+        $scope.contact = new Contact();
+        //$scope.$apply();
+    }
+
+    $scope.isClean = function () {
+        return angular.equals(self.original, $scope.contact);
+    };
+
+    $scope.destroy = function () {
+        self.original.destroy(
+            function () {
+                $scope.$apply(function () {
+                    $location.path('/contacts');
+                });
+            },
+            function (errors) {
+                alert('Could not delete contact!\n' + JSON.parse(errors.responseText)[0].message);
+            }
+        );
+    };
+
+    $scope.save = function () {
+        if ($scope.contact.Id) {
+            $scope.contact.update(function () {
+                $scope.$apply(function () {
+                    $location.path('/view/' + $scope.contact.Id);
+                });
+
+            });
+        } else {
+            Contact.save($scope.contact, function (contact) {
+                var c = contact;
+                $scope.$apply(function () {
+                    $location.path('/view/' + c.Id || c.id);
+                });
+            });
+        }
+    };
+
+    $scope.doCancel = function () {
+        if ($scope.contact.Id) {
+            $location.path('/view/' + $scope.contact.Id);
+        } else {
+            $location.path('/contacts');
+        }
+    };
+});
